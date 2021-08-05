3aad8bced252c46a6ea28710d00f326674faca20
DESKTOP-OSNBSEK\Yellow <hlc>
2021-08-06 00:42:45 +0800

    新增重整自動儲存功能

--- hui_pet/hui.php
+++ hui_pet/hui.php
@@ -12,69 +12,8 @@ echo '<h1>Smile Mi 的動物朋友</h1><hr>';
 //  *         (insert custom) (find custom) 
 //  *         (insert pet) (find pet)
 //  *         (upload icon) (upload picture)
-//  *  
-//  * <script src="https://code.jquery.com/jquery-3.2.1.js">
-// function changeIcon($role) {
-    //     console.log('changeIcon click '.$role);
-    // }
+//  *   **/
 
-    // function insertTalking($role) {
-    //     console.log('insertTalking click '.$role);
-    // }
-
-    // var hui_1 = document.getElementById("change_hui_icon");
-    // var hui_2 = document.getElementById("insert_hui_talking");
-    // var pet_1 = document.getElementById("change_pet_icon");
-    // var pet_2 = document.getElementById("insert_pet_talking");
-    // var user_1 = document.getElementById("change_user_icon");
-    // var user_2 = document.getElementById("insert_user_talking");
-    
-    // if (hui_1.addEventListener){
-    //     hui_1.addEventListener("click", changeIcon(1), true);
-    //     console.log('addEventListener hui_1');
-        
-    // }
-    // else if (hui_1.attachEvent){
-    //     hui_1.attachEvent('onclick', changeIcon(12));
-    //     console.log('attachEvent hui_1');
-    // }
- 
-    // if (hui_2.addEventListener){
-    //     hui_2.addEventListener("click", insertTalking(1), false);
-    //     console.log('addEventListener hui_2');
-    // }
-    // else if (hui_2.attachEvent)
-    //     hui_2.attachEvent('onclick', insertTalking(12));
-        
-    // if (pet_1.addEventListener){
-    //     pet_1.addEventListener("click", changeIcon(2), false);
-    //     console.log('addEventListener pet_1');
-    // }
-    // else if (pet_1.attachEvent)
-    //     pet_1.attachEvent('onclick', changeIcon(22));
-        
-    // if (pet_2.addEventListener){
-    //     pet_2.addEventListener("click", insertTalking(2), false);
-    //     console.log('addEventListener pet_2');
-    // }
-    // else if (pet_2.attachEvent)
-    //     pet_2.attachEvent('onclick', insertTalking(22));
-        
-    // if (user_1.addEventListener){
-    //     user_1.addEventListener("click", changeIcon(3), false);
-    //     console.log('addEventListener user_1');
-    // }
-    // else if (user_1.attachEvent)
-    //     user_1.attachEvent('onclick', changeIcon(32));
-        
-    // if (user_2.addEventListener){
-    //     user_2.addEventListener("click", insertTalking(3), false);
-    //     console.log('addEventListener user_2');
-    // }
-    // else if (user_2.attachEvent)
-    //     user_2.attachEvent('onclick', insertTalking(32));
-
-//  */
 
 $hui_uid = "hui_308"; //人物編號
 $hui_icon = "icon_308"; //人物icon
@@ -122,12 +61,12 @@ function showRecord($list){
 <table style="margin:0 auto" width="100%" id="tb_test">
     <Thead>    
         <tr id="tr_tittle" bgcolor="#DDDDDD">
-            <th width="10%">No.</th>
-            <th width="80%" colspan="2"></th>
-            <th width="10%">編輯</th>
+            <th width="5%">No.</th>
+            <th width="80%" colspan="1"></th>
+            <th width="15%">編輯</th>
         </tr>
     </Thead>
-    <tbody>
+    <tbody id="record_tbody">
  
 
     </tbody>
@@ -137,7 +76,8 @@ function showRecord($list){
 
 <table>
     <tr id="tr_stored">
-        <td><button id="store_record_list" type="button" >儲存對話</button></td>
+        <td><button id="store_record_list" type="button" >上傳記錄</button></td>
+        <td><button id="remove_all" type="button" >清空對話</button></td>
     </tr>
 </table>
 
@@ -148,36 +88,55 @@ function showRecord($list){
     
 <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
 <script type="text/javascript">
-var i = 0;
 
+var huiInfo = new Array(); // 本人資料
+var petInfo = new Array(); // 寵物資料
+var userInfo = new Array();// 客戶資料
+
+var tbRow = 0;
+var countDelete = 0;
+var objChatRecord = new Object();
 
 var tempRecordList = [];
 var recordList = [];
+
+if (window.sessionStorage.getItem('haveRecord')) {
+    console.log("+++ : " + window.sessionStorage.getItem('haveRecord')+ " - " + window.sessionStorage.getItem('temp_chatRecord'));
+    objChatRecord = JSON.parse(window.sessionStorage.getItem('temp_chatRecord'));
+    backupChatRecord(objChatRecord);
+}
+
 $("#insert_hui_talking").click(function() {
-    addOneRawTable(i,"hui");
-    i++;
+    addOneRawTable(tbRow,"hui");
+    // tbRow++;
 });
 $("#insert_pet_talking").click(function() {
-    addOneRawTable(i,"pet");
-    i++;
+    addOneRawTable(tbRow,"pet");
+    // tbRow++;
 });
 $("#insert_user_talking").click(function() {
-    addOneRawTable(i,"user");
-    i++;
+    addOneRawTable(tbRow,"user");
+    // tbRow++;
 });
 
 $("#store_record_list").click(function() {
-    storeRecordList();
+    uploadToDB();
 });
+
+$("#remove_all").click(function() {
+    removeAll();
+});
+
 
 // 新增 row
 function addOneRawTable(i,role) {
-    // console.log("addOneRawTable: " + i);
-    getRoleInfo(role);
+    console.log("addOneRawTable: " + i);
+    let roleInfo = new Array();
+    roleInfo = Array.from(getRoleInfo(role));
     
     $('#tb_test').append('<tr id="tr_newAdd' + i + '"></tr><tr id="tr_newEditor' + i + '"></tr>');
-    $('#tr_newAdd' + i).html('<td width="10%" rowspan="2">' + (i+1) + '</td><td width="10%">' + role + '</td><td width="70%"><input style="width:100%; heigth:100" name="speak_data" value=""/></td><td width="10%"><button id="btn_insert_'+ i +'" onClick="deleteRow('+i+')">刪除</button></td>');
-    $('#tr_newEditor' + i).html('<td width="90%" colspan="3" align="right"><button id="btn_left_'+i+'" onClick="letTextLeft(\''+i+'\',\''+role+'\')">靠左</button><button id="btn_right_'+i+'" onClick="letTextRight(\''+i+'\',\''+role+'\')">靠右</button></td>');
+    $('#tr_newAdd' + i).html('<td width="5%" rowspan="2">' + (i+1) + '</td><td width="80%"><label name="roleInfo" style="display:none">'+roleInfo[1]+'</label><label name="direction" style="display:none">'+true+'</label><img src="icon/'+roleInfo[0]+'" width="30"> : <input style="width:90%; heigth:100" name="speak_data" value=""/></td><td width=15%><button id="btn_insert_'+ i +'" onClick="deleteRow('+i+')">刪除</button></td>');
+    $('#tr_newEditor' + i).html('<td width="95%" colspan="2" align="right"><button id="btn_left_'+i+'" onClick="letTextLeft(\''+i+'\',\''+role+'\')">靠左</button><button id="btn_right_'+i+'" onClick="letTextRight(\''+i+'\',\''+role+'\')">靠右</button></td>');
     // document.getElementById('tb_test').insertRow(-1).innerHTML = '<td>' + (i+1) + '</td><td>' + role + '</td><td><input style="width:100%; heigth:100" name="speak_data" value=""/></td><td><button id="btn_insert_'+ i +'" onClick="deleteRow('+i+')">刪除</button></td>';
     // document.getElementById('tb_test').insertRow(-1).innerHTML = '<td>' + (i+1) + '</td><td colspan="3" align="right"><button id="btn_left_'+i+' onClick="setTextLeft()">靠左</button><button id="btn_right_'+i+' onClick="setTextRight()">靠右</button></td>';
     // document.getElementById('tb_test').insertRow(-1).innerHTML="tr_newEditor";
@@ -185,6 +144,7 @@ function addOneRawTable(i,role) {
     // $('#tb_test').append('<tr id="tr_newAdd' + (i+1) + '"></tr>');
     
     tempRecordList.push("tr_newAdd"+i);
+    tbRow++;
     // console.log("tempRecordList: " + tempRecordList);
 }
 
@@ -206,10 +166,35 @@ function deleteRow(i) {
     }
     // var child=document.getElementById("tr_newAdd"+i);
     // child.parentNode.removeChild(child);
+    countDelete+=1;
 }
 
 function getRoleInfo(role) {
-    // console.log("getRoleInfo: " + role);
+    // role = 客戶編號/寵物編號/Hui編號
+    console.log("array from r: "+ role);
+    /** use role to select mysql and get the all role data */
+    let info = new Array;
+    switch (role) {
+        case "hui":
+            huiInfo.splice(0, 0, "people/woman.png");
+            huiInfo.splice(1, 0, "hui");
+            info = Array.from(huiInfo);
+            break;
+        case "pet":
+            petInfo.splice(0, 0, "pet/cat/sandbox.png");
+            petInfo.splice(1, 0, "pet");
+            info = Array.from(petInfo);
+            break;
+        case "user":
+            userInfo.splice(0, 0, "people/woman2.png");
+            userInfo.splice(1, 0, "user");
+            info = Array.from(userInfo);
+            break;
+        default:
+            break;
+    }
+    console.log("array from : "+ info);
+    return info
 }
 
 // let: 區域變數 var: 全域變數
@@ -222,12 +207,16 @@ function storeRecordList() {
     //     recordList[index].push( document.getElementById(tempRecordList[index]).cells[1] );
     // }
     // console.log(recordList);
-   
+    
     var tbl = document.getElementById("tb_test");
     var numRows = tbl.rows.length-1;
     // console.log("size = "+ numRows +" "+(numRows-1)/2);
-    var obj = new Object();
-    obj.data = new Array();
+    let time = new Date();
+    
+    objChatRecord.data = new Array();
+    // objChatRecord.time = time.getFullYear()+'-'+(time.getMonth()+1)+'-'+time.getDate()+' '+time.getHours()+':'+time.getMinutes()+':'+time.getSeconds()+'T'+time.getTimezoneOffset();
+    // objChatRecord.time = new Date().toLocaleString( {timeZone: 'Asia/Taipei' , hour12: false , year: 'numeric',  hour: '2-digit', minute: '2-digit' ,day: 'numeric'} );
+    objChatRecord.time = new Date().toISOString();
     // 取得 table 內所有資料
     for (let i = 1; i < (numRows); i=i+1) {
         let ID = tbl.rows[i].id;
@@ -246,7 +235,7 @@ function storeRecordList() {
                     //     "role" : cells[j].textContent
                     // })
                     // recordList[i].push( jData ); 
-                    data1.role = cells[j].textContent;
+                    // data1.role = cells[j].textContent;
                     // console.log("role: "+cells[j].textContent);
                     // console.log("list: "+ jData);
                     // console.log("i: "+ i + " cell[j]1: "+cells[j].textContent);
@@ -255,7 +244,9 @@ function storeRecordList() {
                     //     "speak_data" : document.getElementsByName("speak_data")[(i-1)/2].value
                     // })
                     // recordList[i].push( jData );
-                    data1.speak_data =  document.getElementsByName("speak_data")[(i-1)/2].value;
+                    data1.role = document.getElementsByName("roleInfo")[(i-1)/2].textContent;
+                    data1.speak_data = document.getElementsByName("speak_data")[(i-1)/2].value;
+                    data1.direction = document.getElementsByName("direction")[(i-1)/2].textContent;
                     // console.log("input value: "+document.getElementsByName("speak_data")[i].value);        
                     // console.log("list: "+jData);
                     // console.log("i: "+ i + " cell[j]2: " + document.getElementsByName("speak_data")[(i-1)/2].value);
@@ -263,7 +254,7 @@ function storeRecordList() {
 
                 
             }
-            obj['data'].push(data1);
+            objChatRecord['data'].push(data1);
         }
         // var jsonString= JSON.stringify(data);
         // console.log("JS ARRAY: "+i+" "+jsonString);
@@ -273,23 +264,27 @@ function storeRecordList() {
     }
     // obj.data = JSON.stringify(recordList)
     // showRecord(recordList);
-    console.log("list: " + JSON.stringify(obj));
+    
+    console.log("list: " + JSON.stringify(objChatRecord));
     
 }
 
 function letTextLeft(row,role) {
-    console.log("setTextLeft: " + row + role);
-    // event.srcElement.id
-    let tempText = document.getElementsByName("speak_data")[row].value 
-    let row1 = '<tr id="tr_newAdd' + row + '"><td width="10%" rowspan="2">' + (parseInt(row, 10)+1) + '</td><td width="10%">' + role + '</td><td width="70%"><input style="width:100%; heigth:100" name="speak_data" value="'+tempText+'"/></td><td width="10%"><button id="btn_insert_'+ row +'" onClick="deleteRow('+row+')">刪除</button></td></tr>';
+    console.log("setTextLeft: " + row + role + countDelete);
+    let roleInfo = new Array();
+    roleInfo = Array.from(getRoleInfo(role));
+    let tempText = document.getElementsByName("speak_data")[row-countDelete].value;
+    let row1 = '<tr id="tr_newAdd' + row + '"><td width="5%" rowspan="2">' + (parseInt(row, 10)+1) + '</td><td width="80%"><label name="roleInfo" style="display:none">'+roleInfo[1]+'</label><label name="direction" style="display:none">'+true+'</label><img src="icon/'+roleInfo[0]+'" width="30"> : <input style="width:90%; heigth:100" name="speak_data" value="'+tempText+'"/></td><td width="15%"><button id="btn_insert_'+ row +'" onClick="deleteRow('+row+')">刪除</button></td></tr>';
     // $('#btn_insert_'+ i).parent().replaceWith(row1);
     $('#tr_newAdd'+row).replaceWith(row1);
 }
 
 function letTextRight(row,role) {
-    console.log("setTextRight: " + row + role);
-    let tempText = document.getElementsByName("speak_data")[row].value 
-    let row1 = '<tr id="tr_newAdd' + row + '"><td width="10%" rowspan="2">' + (parseInt(row, 10)+1) + '</td><td width="70%"><input style="width:100%; heigth:100" name="speak_data" value="'+tempText+'"/></td><td width="10%">' + role + '</td><td width="10%"><button id="btn_insert_'+ row +'" onClick="deleteRow('+row+')">刪除</button></td></tr>';
+    console.log("setTextRight: " + row + role + countDelete);
+    let roleInfo = new Array();
+    roleInfo = Array.from(getRoleInfo(role));
+    let tempText = document.getElementsByName("speak_data")[row-countDelete].value;
+    let row1 = '<tr id="tr_newAdd' + row + '"><td width="5%" rowspan="2">' + (parseInt(row, 10)+1) + '</td><td width="80%"><label name="roleInfo" style="display:none">'+roleInfo[1]+'</label><label name="direction" style="display:none">'+false+'</label><input style="width:90%; heigth:100" name="speak_data" value="'+tempText+'"/> : <img src="icon/'+roleInfo[0]+'" width="30"></td><td width="15%"><button id="btn_insert_'+ row +'" onClick="deleteRow('+row+')">刪除</button></td></tr>';
     // let row2 = '<td>e'+(i+1)+'</td><td colspan="2" align="right"><button id="btn_left_'+i+' onClick="setTextLeft('+i+')">靠左</button><button id="btn_right_'+i+' onClick="setTextRight('+i+')">靠右</button></td>'
     
     
@@ -299,6 +294,154 @@ function letTextRight(row,role) {
     // $('#tr_newEditor'+i).replaceWith(newtr);
 }
 
+// 清空對話
+function removeAll(){
+    tbRow = 0;
+    countDelete = 0;
+    $("#record_tbody").children().remove()
+}
+
+var loadcount = 0;
+loadcount = window.sessionStorage.getItem('loadcount');
+// console.log('temp_chatRecord: '+ loadcount+ " " + window.sessionStorage.getItem('temp_chatRecord'));
+// 重整時提醒(IE,Chrome 皆可用)
+window.onbeforeunload = function() {
+    let haveRecord = false;
+    if(document.getElementById("tb_test").rows.length>1)
+        haveRecord = true;
+    else{
+        haveRecord = false;
+    }
+    storeRecordList();
+    loadcount++;
+    window.sessionStorage.setItem('haveRecord', haveRecord);
+    window.sessionStorage.setItem('loadcount', loadcount);
+    window.sessionStorage.setItem('temp_chatRecord', JSON.stringify(objChatRecord));
+
+
+
+  return "on";
+};
+// 關閉時提醒(chrome不適用，僅IE可用)
+// window.onunload = function(){
+//    return "leave?"
+// }
+
+
+// init載入重整前資訊
+function backupChatRecord(obj){
+    console.log("backupChatRecord : "+ JSON.stringify(obj));
+    console.log("objChatRecord time: " + obj.time );
+    console.log("objChatRecord lengh: " + obj.data.length );
+    console.log("objChatRecord lengh: " + Object.keys(obj.data).length );
+    let index = Object.keys(obj.data).length;
+    tbRow = index===0? 0 : index - 1;
+    for (let i = 0; i < index; i++) {
+        let role = obj.data[i].role;
+        addOneRawTable(i,role);
+        
+        document.getElementsByName("speak_data")[i].value = [obj.data[i].speak_data];
+
+        console.log("direction: "+obj.data[i].direction);
+        if(obj.data[i].direction == "true")
+            letTextLeft(i,role);
+        else
+            letTextRight(i,role);
+
+    }
+
+}
+
+function uploadToDB(){
+    storeRecordList();
+    
+}
+
+
+
+
+function formatDate(date, format, utc) {
+    var MMMM = ["\x00", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var MMM = ["\x01", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var dddd = ["\x02", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var ddd = ["\x03", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    function ii(i, len) {
+        var s = i + "";
+        len = len || 2;
+        while (s.length < len) s = "0" + s;
+        return s;
+    }
+
+    var y = utc ? date.getUTCFullYear() : date.getFullYear();
+    format = format.replace(/(^|[^\\])yyyy+/g, "$1" + y);
+    format = format.replace(/(^|[^\\])yy/g, "$1" + y.toString().substr(2, 2));
+    format = format.replace(/(^|[^\\])y/g, "$1" + y);
+
+    var M = (utc ? date.getUTCMonth() : date.getMonth()) + 1;
+    format = format.replace(/(^|[^\\])MMMM+/g, "$1" + MMMM[0]);
+    format = format.replace(/(^|[^\\])MMM/g, "$1" + MMM[0]);
+    format = format.replace(/(^|[^\\])MM/g, "$1" + ii(M));
+    format = format.replace(/(^|[^\\])M/g, "$1" + M);
+
+    var d = utc ? date.getUTCDate() : date.getDate();
+    format = format.replace(/(^|[^\\])dddd+/g, "$1" + dddd[0]);
+    format = format.replace(/(^|[^\\])ddd/g, "$1" + ddd[0]);
+    format = format.replace(/(^|[^\\])dd/g, "$1" + ii(d));
+    format = format.replace(/(^|[^\\])d/g, "$1" + d);
+
+    var H = utc ? date.getUTCHours() : date.getHours();
+    format = format.replace(/(^|[^\\])HH+/g, "$1" + ii(H));
+    format = format.replace(/(^|[^\\])H/g, "$1" + H);
+
+    var h = H > 12 ? H - 12 : H == 0 ? 12 : H;
+    format = format.replace(/(^|[^\\])hh+/g, "$1" + ii(h));
+    format = format.replace(/(^|[^\\])h/g, "$1" + h);
+
+    var m = utc ? date.getUTCMinutes() : date.getMinutes();
+    format = format.replace(/(^|[^\\])mm+/g, "$1" + ii(m));
+    format = format.replace(/(^|[^\\])m/g, "$1" + m);
+
+    var s = utc ? date.getUTCSeconds() : date.getSeconds();
+    format = format.replace(/(^|[^\\])ss+/g, "$1" + ii(s));
+    format = format.replace(/(^|[^\\])s/g, "$1" + s);
+
+    var f = utc ? date.getUTCMilliseconds() : date.getMilliseconds();
+    format = format.replace(/(^|[^\\])fff+/g, "$1" + ii(f, 3));
+    f = Math.round(f / 10);
+    format = format.replace(/(^|[^\\])ff/g, "$1" + ii(f));
+    f = Math.round(f / 10);
+    format = format.replace(/(^|[^\\])f/g, "$1" + f);
+
+    var T = H < 12 ? "AM" : "PM";
+    format = format.replace(/(^|[^\\])TT+/g, "$1" + T);
+    format = format.replace(/(^|[^\\])T/g, "$1" + T.charAt(0));
+
+    var t = T.toLowerCase();
+    format = format.replace(/(^|[^\\])tt+/g, "$1" + t);
+    format = format.replace(/(^|[^\\])t/g, "$1" + t.charAt(0));
+
+    var tz = -date.getTimezoneOffset();
+    var K = utc || !tz ? "Z" : tz > 0 ? "+" : "-";
+    if (!utc) {
+        tz = Math.abs(tz);
+        var tzHrs = Math.floor(tz / 60);
+        var tzMin = tz % 60;
+        K += ii(tzHrs) + ":" + ii(tzMin);
+    }
+    format = format.replace(/(^|[^\\])K/g, "$1" + K);
+
+    var day = (utc ? date.getUTCDay() : date.getDay()) + 1;
+    format = format.replace(new RegExp(dddd[0], "g"), dddd[day]);
+    format = format.replace(new RegExp(ddd[0], "g"), ddd[day]);
+
+    format = format.replace(new RegExp(MMMM[0], "g"), MMMM[M]);
+    format = format.replace(new RegExp(MMM[0], "g"), MMM[M]);
+
+    format = format.replace(/\\(.)/g, "$1");
+
+    return format;
+};
 </script>
 
 
